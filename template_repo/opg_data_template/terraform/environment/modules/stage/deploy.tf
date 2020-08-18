3c8d8b482110dabb95b69b53781c04bfd6443dfa
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-08-18 13:54:34 +0100

    In 335 - add shared code for pact
    - add pre commit hooks
    - add pact shared code
    - other tidying

---
+++ template_repo/opg_data_template/terraform/environment/modules/stage/deploy.tf
@@ -0,0 +1,23 @@
+locals {
+  openapi_sha = substr(replace(base64sha256(data.local_file.openapispec.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
+  //Modify here for new version - uncomment below and modify accordingly
+  //  openapi_sha               = var.openapi_version == "v1" ? "fixed_variable" : substr(replace(base64sha256(data.local_file.openapispec.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
+}
+
+data "local_file" "openapispec" {
+  filename = "../../lambda_functions/${var.openapi_version}/openapi/${var.api_name}-openapi.yml"
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = var.rest_api.id
+  depends_on  = [var.domain_name]
+  variables = {
+    // Force a deploy only when content has changed
+    stage_version      = var.openapi_version
+    content_api_sha    = local.openapi_sha
+    lambda_version_sha = var.template_lambda.source_code_hash
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
