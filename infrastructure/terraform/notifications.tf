ec89a37ea6bd54e60b711ff6a957e3ed030313cd
Andy Price <sixdaysandy@gmail.com>
2021-03-05 14:18:32 +0000

    SP-607 - Enable Encryption at Rest for Config SNS Topic in line with AWS Security best practices. (#45)

--- infrastructure/terraform/notifications.tf
+++ infrastructure/terraform/notifications.tf
@@ -1,5 +1,44 @@
 resource "aws_sns_topic" "rest_api" {
-  name = "rest-api"
+  name              = "rest-api"
+  kms_master_key_id = aws_kms_key.cloudwatch_sns.key_id
+  tags              = local.default_tags
+}
+
+resource "aws_kms_key" "cloudwatch_sns" {
+  description             = "KMS Key for Cloudwatch related SNS Encryption for Integration Slack Notifications"
+  deletion_window_in_days = 10
+  policy                  = data.aws_iam_policy_document.cloudwatch_sns_kms.json
+  enable_key_rotation     = true
+  tags                    = local.default_tags
+}
+
+data "aws_iam_policy_document" "cloudwatch_sns_kms" {
+  statement {
+    sid       = "Allow Key to be used for Encryption"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+  }
+  statement {
+    sid       = "Enable Root account permissions on Key"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.account.account_id}:root",
+      ]
+    }
+  }
 }
 
 module "notify_slack" {
