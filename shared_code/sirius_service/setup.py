d2d14772dbf7bb0b238355f388174be3038c0063
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2022-02-24 17:11:30 +0000

    LPAL-550 Increment version

--- shared_code/sirius_service/setup.py
+++ shared_code/sirius_service/setup.py
@@ -5,7 +5,7 @@ with open("sirius_service.md", "r") as fh:
 
 setuptools.setup(
     name="opg_sirius_service",
-    version="2.0.0",
+    version="2.0.1",
     author="OPG",
     author_email="opg-integrations@digital.justice.gov.uk",
     description="Sirius Service",
