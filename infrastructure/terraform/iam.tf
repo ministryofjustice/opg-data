403ddbc1b0d60af7ca1f5d5349c301ef4d573bb5
radhia shah <radhia.shah@justice.gov.uk>
2026-03-30 15:15:25 +0100

    LPAL-1973 | trivy remediation -adding oidc roles for serve sirius ci integrations

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "serve_ci_sirius" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::631181914621:role/oidc-serve-*"]
+      identifiers = [for env in keys(local.accounts) : "arn:aws:iam::631181914621:role/oidc-serve-${env}"]
     }
 
     actions = ["sts:AssumeRole"]
